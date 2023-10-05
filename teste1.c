a0df069a433cc2e3f174e8b72e9928856570879b
bluemetti <143762698+bluemetti@users.noreply.github.com>
2023-10-05 11:26:50 -0300

    update

--- teste1.c
+++ teste1.c
@@ -1,28 +1,12 @@
 #include "listase.h"
 
-void destroi_impar(tp_listase **l){
-    tp_listase *atu, *ant;
-    atu = *l;
-    while ((atu->info % 2) != 0) {
-        *l = atu->prox;
-        free(atu);
-        atu = *l;
-    }
-    while(atu!=NULL){
-        ant = atu;
-        atu = atu->prox;
-        if(atu->info % 2 != 0){
-            ant->prox = atu->prox;
-            free(atu);
-            atu = ant->prox;
-        }
-    }
-}
+
+
+
 
 int main(){
-    tp_listase *l, *l1;
+    tp_listase *l;
     l = inicializa_lista();
-    l1 = inicializa_lista();
     insere_listase_no_fim(&l, 5);
     insere_listase_no_fim(&l, 10);
     insere_listase_no_fim(&l, 15);
