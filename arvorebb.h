5f84838e21246a3e5e3eca54f8cd742356dcb52e
bluemetti <143762698+bluemetti@users.noreply.github.com>
2023-11-14 12:11:50 -0300

    cont

--- arvorebb.h
+++ arvorebb.h
@@ -1,5 +1,5 @@
-#ifndef FILA_H
-#define FILA_H
+#ifndef ARVOREBB_H
+#define ARVOREBB_H
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -24,6 +24,54 @@ tp_no *aloca_no(){
     tp_no *no;
     no = (tp_no*)malloc(sizeof(tp_no));
     return no;
+}
+int insere_no(tp_arvore *raiz, tp_item e){
+    tp_no *pai = NULL, *novo, *p=*raiz;
+    novo=aloca_no();                            //endereco do novo elemento da arvore
+    if(!novo)return 0;                          //se nao conseguir alocar, retorna 0
+    novo->info = e;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    while (p!=NULL){                    //caminha na direcao da posicao q sera inserido o novo no
+        pai=p;
+        if (e < p->info){
+            p=p->esq;
+        }else p = p->dir;
+    }
+    if (pai!=NULL){
+        if (e < pai->info)pai->esq=novo;
+        else pai->dir=novo;
+    }else *raiz=novo;
+
+    return 1;
+}
+void pre_ordem(tp_no *p){
+    if(p!=NULL){
+        printf("\n%d", p->info);
+        pre_ordem(p->esq);
+        pre_ordem(p->dir);
+    }
+}
+void em_ordem(tp_no *p){
+    if(p!=NULL){
+        em_ordem(p->esq);
+        printf("\n%d", p->info);
+        em_ordem(p->dir);
+    }
+}
+void pos_ordem(tp_no *p){
+    if(p!=NULL){
+        em_ordem(p->esq);
+        em_ordem(p->dir);
+        printf("\n%d", p->info);
+    }
+}
+int niveis_arvore(tp_arvore raiz){
+    if (raiz==NULL)return 0;
+    int alt_esq = niveis_arvore(raiz->esq);
+    int alt_dir = niveis_arvore(raiz->esq);
+    if(alt_esq > alt_dir)return alt_esq+1;
+    return alt_dir+1;
 }
 
 
--- testee.c
+++ testee.c
@@ -3,5 +3,32 @@
 int main(){
     tp_arvore arvre;
     arvre = inicializa_arvore();
-    if (arvore_vazia(arvre)==1)printf("AAAAAAAA");
+    insere_no(&arvre, 6);
+    insere_no(&arvre, 3);
+    insere_no(&arvre, 9);
+    insere_no(&arvre, 1);
+    insere_no(&arvre, 10);
+    insere_no(&arvre, 22);
+    insere_no(&arvre, 33);
+    insere_no(&arvre, 10);
+    insere_no(&arvre, 75);
+    insere_no(&arvre, 6);
+    insere_no(&arvre, 3);
+    insere_no(&arvre, 9);
+    insere_no(&arvre, 1);
+    insere_no(&arvre, 10);
+    insere_no(&arvre, 22);
+    insere_no(&arvre, 33);
+    insere_no(&arvre, 10);
+    insere_no(&arvre, 75);
+    insere_no(&arvre, 3);
+    insere_no(&arvre, 9);
+    insere_no(&arvre, 1);
+    insere_no(&arvre, 10);
+    insere_no(&arvre, 22);
+    insere_no(&arvre, 33);
+    insere_no(&arvre, 10);
+    insere_no(&arvre, 75);
+    pos_ordem(arvre);
+    printf("\n\naltura: %d", niveis_arvore(arvre));
 }
