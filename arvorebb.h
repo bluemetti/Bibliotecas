7e3b842186a06357e65173eb6970f25972a1e031
bluemetti <143762698+bluemetti@users.noreply.github.com>
2023-11-16 11:15:03 -0300

    arvorebb fim

--- arvorebb.h
+++ arvorebb.h
@@ -1,85 +1,195 @@
-#ifndef ARVOREBB_H
-#define ARVOREBB_H
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef int tp_item;
-
+#include<stdlib.h>
+#include<stdio.h>
+	
+typedef int tp_item;	
+	
 typedef struct tp_no{
-    struct tp_no *esq;
-    tp_item info;
-    struct tp_no *dir;
-}tp_no;
+	struct tp_no *esq;
+	tp_item info;
+	struct tp_no *dir;
+} tp_no; 
 
-typedef tp_no *tp_arvore;
+typedef tp_no * tp_arvore;
 
-tp_arvore inicializa_arvore(){
-    return NULL;
+tp_arvore inicializa_arvore()
+{
+ return NULL;
 }
-int arvore_vazia(tp_arvore raiz){
-    if (raiz==NULL)return 1;
-    return 0;
+
+
+int arvore_vazia(tp_arvore raiz)
+{
+	if(raiz==NULL) return 1;
+	else return 0;
 }
-tp_no *aloca_no(){
-    tp_no *no;
-    no = (tp_no*)malloc(sizeof(tp_no));
-    return no;
+
+tp_no *aloca_no() //aloca e retorna o endereço
+{
+	tp_no *no;
+	no=(tp_no*)malloc(sizeof(tp_no));
+	return no;
 }
-int insere_no(tp_arvore *raiz, tp_item e){
-    tp_no *pai = NULL, *novo, *p=*raiz;
-    novo=aloca_no();                            //endereco do novo elemento da arvore
-    if(!novo)return 0;                          //se nao conseguir alocar, retorna 0
-    novo->info = e;
-    novo->esq = NULL;
-    novo->dir = NULL;
-    while (p!=NULL){                    //caminha na direcao da posicao q sera inserido o novo no
-        pai=p;
-        if (e < p->info){
-            p=p->esq;
-        }else p = p->dir;
+
+int insere_no(tp_arvore *raiz, tp_item e)
+{	tp_no *pai=NULL, *novo, *p;  //*p=ponteiro auxiliar
+	novo=aloca_no();//cria um novo elemento e coloca o endereço dele no novo
+	if(!novo) return 0; //não deu para alocar (novo==null)
+
+	novo->info=e;
+	novo->esq=NULL;
+	novo->dir=NULL;
+		
+	p = *raiz;
+	while(p!=NULL) 
+	{//busca a posição onde será inserido o novo nó
+		pai=p;
+		if( e < p->info)
+			p=p->esq;
+		else
+			p=p->dir;
+	}	
+	
+	if(pai!=NULL) //Qual ponteiro do pai (esq ou dir) irá apontar para o novo nó
+	{	
+		if ( e < pai->info)
+		   pai->esq=novo;
+		else 
+		   pai->dir=novo;
+	}
+	else 
+		*raiz=novo;
+
+	return 1;	
+}
+
+void pre_ordem(tp_no *p) //imprime os elementos em pre-ordem
+{
+	if(p!=NULL)
+	{	printf("\n%d\n",p->info);
+		pre_ordem(p->esq);
+		pre_ordem(p->dir);
+	}
+}	
+void em_ordem (tp_no *p) {  //imprime os elementos em ordem
+  if (p != NULL) {
+	em_ordem (p->esq);
+	printf("\n%d\n",p->info);
+	em_ordem (p->dir);
     }
-    if (pai!=NULL){
-        if (e < pai->info)pai->esq=novo;
-        else pai->dir=novo;
-    }else *raiz=novo;
+}
 
-    return 1;
+void pos_ordem (tp_no *p) { //imprime os elementos em pós-ordem 
+	 if (p != NULL) {
+	 pos_ordem (p->esq);
+	 pos_ordem (p->dir);
+	printf("\n%d\n",p->info);
 }
-void pre_ordem(tp_no *p){
-    if(p!=NULL){
-        printf("\n%d", p->info);
-        pre_ordem(p->esq);
-        pre_ordem(p->dir);
-    }
-}
-void em_ordem(tp_no *p){
-    if(p!=NULL){
-        em_ordem(p->esq);
-        printf("\n%d", p->info);
-        em_ordem(p->dir);
-    }
-}
-void pos_ordem(tp_no *p){
-    if(p!=NULL){
-        em_ordem(p->esq);
-        em_ordem(p->dir);
-        printf("\n%d", p->info);
-    }
-}
-int niveis_arvore(tp_arvore raiz){
-    if (raiz==NULL)return 0;
-    int alt_esq = niveis_arvore(raiz->esq);
-    int alt_dir = niveis_arvore(raiz->esq);
-    if(alt_esq > alt_dir)return alt_esq+1;
-    return alt_dir+1;
-}
-int numero_nos(tp_arvore raiz){
-    if (raiz==NULL)return 0;
-    int nos_esq = numero_nos(raiz->esq);
-    int nos_dir = numero_nos(raiz->esq);
-    return (nos_esq+nos_dir+1);
 }
 
 
+tp_no* busca_no (tp_no *p, tp_item e) {
+	while (p != NULL) { 
+		if (e < p->info) 
+			p = p->esq; 
+		else 
+			if (e > p->info) 
+				p = p->dir; 
+			else 
+				return (p);
+	};
+ 	return (NULL); 
+}
+tp_arvore busca_no_rec (tp_arvore p, tp_item e) {
+	if (p != NULL) { 
+		if (e < p->info) 
+			return busca_no_rec(p->esq, e); 
+		else 
+			if (e > p->info) 
+				return busca_no_rec(p->dir, e); 
+			else 
+				return (p);
+	}
+	else
+ 		return (NULL); 
+}
 
-#endif
+int altura_arvore(tp_arvore raiz){
+	if(raiz == NULL) return 0;
+	int alt_esq = altura_arvore(raiz->esq);
+	int alt_dir = altura_arvore(raiz->dir);
+	if(alt_esq > alt_dir){
+		return alt_esq + 1;
+	}else{
+		return alt_dir + 1;
+	}
+}
+
+
+
+int qtde_nos(tp_arvore raiz){
+	if(raiz == NULL) return 0;
+	int tot_esq = qtde_nos(raiz->esq);
+	int tot_dir = qtde_nos(raiz->dir);
+	return (tot_esq + tot_dir + 1);
+}
+
+
+int remove_no (tp_arvore *raiz, tp_item e ) {
+	tp_no  *p=*raiz,   *ant=NULL,   *sub,   *pai,   *filho;
+	while (p!=NULL && p->info!=e) {
+		ant=p;
+		if (e < p->info)
+			p = p->esq;
+		else
+			p = p->dir;
+	}
+	if (p==NULL) 
+		return  0;
+
+	if (p->esq==NULL)
+		sub=p->dir;
+	else
+		if (p->dir==NULL)
+			sub=p->esq;
+		else {
+	
+			pai=p; sub=p->dir; filho=sub->esq;
+			while (filho!=NULL) {
+				pai=sub;  sub=filho;  filho=sub->esq;
+			}
+			if (pai!=p) {
+				pai->esq=sub->dir;
+				sub->dir=p->dir;
+			}
+			sub->esq=p->esq;
+		}
+		if (ant==NULL)
+			*raiz=sub; /* p era raiz */
+		else
+			if (p==ant->esq)
+				ant->esq=sub;
+			else
+				ant->dir=sub;
+		free(p);
+	return 1;
+}
+
+
+void destroi_no(tp_no *no){
+   if(no==NULL) return;
+   destroi_no(no->esq);
+   destroi_no(no->dir);
+   printf("\n %d ", no->info);
+   free(no);
+   no = NULL;
+   return;
+}
+
+void destroi_arvore(tp_arvore *raiz){
+   if(*raiz==NULL) return;
+   destroi_no(*raiz);
+   free(*raiz);
+   *raiz=NULL;
+}
+
+
--- testee.c
+++ testee.c
@@ -30,5 +30,5 @@ int main(){
     insere_no(&arvre, 10);
     insere_no(&arvre, 75);
     pos_ordem(arvre);
-    printf("\n\naltura: %d", numero_nos(arvre));
+    printf("\n\naltura: %d", qtde_nos(arvre));
 }
